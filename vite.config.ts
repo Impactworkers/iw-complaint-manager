ddcfe06a253c0c39ec0e4fc63c17692954b8fce0
Fara Akhatova <fara.akhatova@thoughtworks.com>
2024-05-22 15:59:30 -0600

    Add condition to check for mkcert certificates

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import fs from "fs";
 import path from "path";
-installGlobals();
 
-const key = fs.readFileSync(path.resolve(__dirname, ".cert/localhost-key.pem"));
-const cert = fs.readFileSync(path.resolve(__dirname, ".cert/localhost.pem"));
+// Paths to the certificate files
+const keyPath = path.resolve(__dirname, ".cert", "localhost-key.pem");
+const certPath = path.resolve(__dirname, ".cert", "localhost.pem");
+
+// Check if the certificate files exist
+const useHttps = fs.existsSync(keyPath) && fs.existsSync(certPath);
+
+// Load SSL certificate and key if they exist
+const httpsConfig = useHttps
+    ? {
+          key: fs.readFileSync(keyPath),
+          cert: fs.readFileSync(certPath)
+      }
+    : undefined;
+
 export default defineConfig({
     plugins: [remix(), tsconfigPaths()],
     server: {
-        https: {
-            key,
-            cert
-        },
+        https: httpsConfig,
         host: true // This allows the server to be accessed externally
     }
 });
