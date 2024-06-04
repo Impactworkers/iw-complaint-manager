dad701aea95592161822e281d48b2eee21d7c6c1
Raina Huerta <raina.huerta@thoughtworks.com>
2024-06-04 14:43:34 -0500

    [69]Raina/Fara/Justin nextjs initialization

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'cypress'
- 
+import { defineConfig } from "cypress";
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    supportFile:false,
+    supportFile: false,
   },
-})
+});
