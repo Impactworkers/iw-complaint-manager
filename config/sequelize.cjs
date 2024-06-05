90fc02518c169e310508f29701d7152358eb10ad
Charles Woods <charleswoods@Charless-MacBook-Pro.local>
2024-06-05 16:19:07 -0500

    Paul/Yooboo/Charles[#69] Added sequelize configuration, dummy model, update config.yml

---
+++ config/sequelize.cjs
@@ -0,0 +1,14 @@
+const { sequelize } = require("../models/index.js");
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(`Connection successful.`);
+
+        await sequelize.sync({ alter: true });
+    } catch (error) {
+        console.error(`Unable to connect to database:`, error);
+    }
+})();
+
+module.exports = { sequelize };
