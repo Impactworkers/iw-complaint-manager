39c094241c172952e7ac21416d9b3d607bae02bb
Charles Woods <charleswoods@Charless-MacBook-Pro.local>
2024-06-05 16:33:28 -0500

    Paul/Yooboo/Charles[#69]fixed error in layout.tsx, and linting issue

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -22,14 +22,14 @@ export const Page: React.FC = () => {
             <section className="storybook-page">
                 <h2>Pages in Storybook</h2>
                 <p>
-                    We recommend building UIs with a{" "}
+                    We recommend building UIs with a
                     <a
                         href="https://componentdriven.org"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         <strong>component-driven</strong>
-                    </a>{" "}
+                    </a>
                     process starting with atomic components and ending with
                     pages.
                 </p>
@@ -51,7 +51,8 @@ export const Page: React.FC = () => {
                     </li>
                 </ul>
                 <p>
-                    Get a guided tutorial on component-driven development at{" "}
+                    Get a guided tutorial on component-driven development
+                    at
                     <a
                         href="https://storybook.js.org/tutorials/"
                         target="_blank"
@@ -59,7 +60,7 @@ export const Page: React.FC = () => {
                     >
                         Storybook tutorials
                     </a>
-                    . Read more in the{" "}
+                    . Read more in the
                     <a
                         href="https://storybook.js.org/docs"
                         target="_blank"
@@ -71,7 +72,7 @@ export const Page: React.FC = () => {
                 </p>
                 <div className="tip-wrapper">
                     <span className="tip">Tip</span> Adjust the width of the
-                    canvas with the{" "}
+                    canvas with the
                     <svg
                         width="10"
                         height="10"
