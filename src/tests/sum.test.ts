77a1e547fb0fed5036e7b83f42b2909eeacd7af9
Paul Thompson <paul.thompson1@thoughtworks.com>
2024-06-07 11:40:08 -0500

    fix: Charles\Paul [#69] Resolved prettier errors in sum.test.ts

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -1,8 +1,8 @@
-import {expect, test} from '@jest/globals';
-import sum from './sum';
+import { expect, test } from "@jest/globals";
+import sum from "./sum";
 
-describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-      expect(sum(1, 2)).toBe(3);
+describe("sum module", () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(sum(1, 2)).toBe(3);
     });
 });
