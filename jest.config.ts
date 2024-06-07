65859e33126484f7bf46c34676d64a4648b8a86a
Paul Thompson <paul.thompson1@thoughtworks.com>
2024-06-07 11:28:03 -0500

    chore: Charles\Paul [#69] Configured Jest to use TypeScript

--- jest.config.ts
+++ jest.config.ts
@@ -8,10 +8,11 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config: Config = {
+    preset: 'ts-jest',
     coverageProvider: "v8",
-    testEnvironment: "jsdom"
+    testEnvironment: "node",
     // Add more setup options before each test is run
-    // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    setupFilesAfterEnv: ['./jest.setup.ts'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
