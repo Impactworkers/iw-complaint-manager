dd6218b11fbde741b62c27e68fd766485ad88836
Paul Thompson <paul.thompson1@thoughtworks.com>
2024-06-10 10:52:32 -0500

    chore: Yooboo\Paul [#69] Modified React imports to only import necessary modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
     children
 }: Readonly<{
-    children: React.ReactNode;
+    children: ReactNode;
 }>) {
     return (
         <html lang="en">
