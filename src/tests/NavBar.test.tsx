5db02192b5125a9fc0ad7a7e3374a0b50b0a58fe
justin anthony <justin.anthony@thoughtworks.com>
2024-06-18 10:37:16 -0600

    Add expect global import

--- src/tests/NavBar.test.tsx
+++ src/tests/NavBar.test.tsx
@@ -1,10 +1,11 @@
-import NavBar from "@stories/NavBar"
-import { render } from "@testing-library/react"
+import NavBar from "@stories/NavBar";
+import { render } from "@testing-library/react";
+import { expect } from "@jest/globals";
 
-describe("NavBar",()=>{
-    it("should allow a nav structure to be passed in",()=>{
+describe("NavBar", () => {
+    it("should allow a nav structure to be passed in", () => {
         const sampleStructure = {};
-        const component = render(<NavBar structure={sampleStructure}/>)
+        const component = render(<NavBar structure={sampleStructure} />);
         expect(true).toBeTruthy();
-    })
-})
+    });
+});
