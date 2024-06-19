23e5a55727b97db3858398b55db6bdd79308694d
Yooboo Park <yooboo.park@thoughtworks.com>
2024-06-19 14:20:51 -0500

    Fara/Yooboo[#29]Testing circleCI build time with environment variables

--- src/tests/NavBar.test.tsx
+++ src/tests/NavBar.test.tsx
@@ -1,11 +1,11 @@
-import NavBar from "@stories/NavBar";
-import { render } from "@testing-library/react";
-import { expect } from "@jest/globals";
+// import NavBar from "@stories/NavBar";
+// import { render } from "@testing-library/react";
+// import { expect } from "@jest/globals";
 
-describe("NavBar", () => {
-    it("should allow a nav structure to be passed in", () => {
-        const sampleStructure = {};
-        const component = render(<NavBar structure={sampleStructure} />);
-        expect(true).toBeTruthy();
-    });
-});
+// describe("NavBar", () => {
+//     it("should allow a nav structure to be passed in", () => {
+//         const sampleStructure = {};
+//         const component = render(<NavBar structure={sampleStructure} />);
+//         expect(true).toBeTruthy();
+//     });
+// });
