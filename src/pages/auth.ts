8d45c0ceb53105eec9dcb7c29fe5c61a3df40f17
Yooboo Park <yooboo.park@thoughtworks.com>
2024-06-19 12:47:46 -0500

    Fara/Yooboo[#29]Edited auth.ts to properly redirect after signin to okta

--- src/pages/auth.ts
+++ src/pages/auth.ts
@@ -2,9 +2,9 @@
 import { OktaAuth } from "@okta/okta-auth-js";
 
 const oktaAuth = new OktaAuth({
-    issuer: "ksjdbksbflksdnlkdsf",
-    clientId: "kjdbfkjebfjebfj",
-    redirectUri: "https://iwcm-dev-4aa83826f5da.herokuapp.com",
+    issuer: "fakeissuer",
+    clientId: "fakeid",
+    redirectUri: "https://localhost:3000/login/callback",
     scopes: ["openid", "profile", "email"],
     pkce: true,
     responseType: "code"
