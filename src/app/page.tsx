730c555ff246c025bd642fbcc3ab443878954b37
Raina Huerta <raina.huerta@thoughtworks.com>
2024-07-01 14:58:47 -0500

    [#29] fixed linting issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,14 +19,12 @@ export default function Page() {
         }
     }, [isHerokuEnv, oktaAuth, authState]);
 
-    return (
-        isHerokuEnv ? (
-            <main className="flex min-h-screen flex-col p-6">
-                <h1 className="text-4xl font-bold">Hello, world!</h1>
-                <p className="text-lg mt-4">Welcome to Complaint Manager 2.0</p>
-            </main>
-        ) : (
-            <p>Local Environment: No SSO</p>
-        )
+    return isHerokuEnv ? (
+        <main className="flex min-h-screen flex-col p-6">
+            <h1 className="text-4xl font-bold">Hello, world!</h1>
+            <p className="text-lg mt-4">Welcome to Complaint Manager 2.0</p>
+        </main>
+    ) : (
+        <p>Local Environment: No SSO</p>
     );
-};
+}
