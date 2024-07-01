1c01adfee89778fa05298bf2a676b566cd2b4634
Raina Huerta <raina.huerta@thoughtworks.com>
2024-07-01 11:12:52 -0500

    chore: [#56] added interaction testing for navbar story. Added storybook to eslintrc
    Co-authored-by: Paul Thompson <paul.thompson1@thoughtworks.com>

--- src/components/NavigationBar/NavigationBar.stories.ts
+++ src/components/NavigationBar/NavigationBar.stories.ts
@@ -1,8 +1,10 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { NavigationBar } from "./NavigationBar";
+import { within, userEvent, expect } from "@storybook/test";
 
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-    title: "Example/NavigationBar",
+    title: "Component/NavigationBar",
     component: NavigationBar,
     tags: ["autodocs"],
     parameters: {
@@ -11,11 +13,25 @@ const meta = {
 } satisfies Meta<typeof NavigationBar>;
 
 export default meta;
-
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const NavBarClosed: Story = {
     args: {
         text: ["Cases", "Admin Portal"]
+    }
+};
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const NavBarOpen: Story = {
+    args: {
+        text: ["Cases", "Admin Portal"]
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        const navBarButton = canvas.getByRole("button", {
+            name: /Navigation Bar/i
+        });
+        await expect(navBarButton).toBeInTheDocument();
+        await userEvent.click(navBarButton);
     }
 };
