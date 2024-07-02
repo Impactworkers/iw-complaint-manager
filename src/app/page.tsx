df0f97a7074620a29696dbc24d71b0dc43907360
Raina Huerta <raina.huerta@thoughtworks.com>
2024-07-02 16:07:38 -0500

    Fara/Yooboo/Raina [#29] fixed hyrdation issue with client rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useOktaAuth } from "@okta/okta-react";
 
 export default function Page() {
     const isHerokuEnv = process.env.APP_ENV === "development";
-
     const { oktaAuth, authState } = useOktaAuth();
+    const [isClient, setIsClient] = useState(false);
+
     useEffect(() => {
+        setIsClient(true);
         if (isHerokuEnv) {
             const handleRedirect = async () => {
                 if (!((await oktaAuth.isAuthenticated()) && authState)) {
@@ -19,7 +21,7 @@ export default function Page() {
         }
     }, [isHerokuEnv, oktaAuth, authState]);
 
-    return isHerokuEnv ? (
+    return isHerokuEnv && isClient ? (
         <main className="flex min-h-screen flex-col p-6">
             <h1 className="text-4xl font-bold">Hello, world!</h1>
             <p className="text-lg mt-4">Welcome to Complaint Manager 2.0</p>
