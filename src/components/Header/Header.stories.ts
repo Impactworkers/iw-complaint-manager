ea396bf82562687def4ce46b9a9ef105dbec334e
Paul Thompson <159827724+paul-thompson1@users.noreply.github.com>
2024-07-03 18:05:43 -0500

    Updated Header.stories.ts
    Created basic storybook story for the header component

--- src/components/Header/Header.stories.ts
+++ src/components/Header/Header.stories.ts
@@ -1,1 +1,16 @@
-export default {};
+import { Meta, StoryObj } from "@storybook/react";
+import Header from "./Header";
+
+const meta = {
+    title: "Component/Header",
+    component: Header,
+    tags: ["autodocs"],
+    parameters: {
+        layout: "centered"
+    }
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const AppBar: Story = {};
