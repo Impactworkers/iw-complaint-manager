0540d7b84e65d9c6e6298abdbfd0e580906a7d94
Raina Huerta <raina.huerta@thoughtworks.com>
2024-07-03 11:26:26 -0500

    [#29] add check for if redirect has already been attempted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,27 @@ export default function Page() {
     const isHerokuEnv = process.env.NEXT_PUBLIC_APP_ENV === "development";
     const { oktaAuth, authState } = useOktaAuth();
     const [isClient, setIsClient] = useState(false);
+    const [redirectAttempted, setRedirectAttempted] = useState(false);
 
     useEffect(() => {
         setIsClient(true);
-        if (isHerokuEnv) {
+        if (isHerokuEnv && !redirectAttempted) {
             const handleRedirect = async () => {
-                if (!((await oktaAuth.isAuthenticated()) && authState)) {
+                // Consider using authState.isAuthenticated if it's reliable
+                const isAuthenticated =
+                    authState?.isAuthenticated ||
+                    (await oktaAuth.isAuthenticated());
+                if (!isAuthenticated) {
+                    setRedirectAttempted(true); // Prevent further attempts
                     await oktaAuth.signInWithRedirect();
                 }
             };
 
             handleRedirect();
         }
-    }, [isHerokuEnv, oktaAuth, authState]);
+    }, [isHerokuEnv, oktaAuth, authState, redirectAttempted]);
 
-    return isHerokuEnv && isClient ? (
+    return isHerokuEnv && isClient && authState?.isAuthenticated ? (
         <main className="flex min-h-screen flex-col p-6">
             <h1 className="text-4xl font-bold">Hello, world!</h1>
             <p className="text-lg mt-4">Welcome to Complaint Manager 2.0</p>
