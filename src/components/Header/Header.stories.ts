591bf6604ea929c046c6fa47b620fefb50d18446
justin anthony <justin.anthony@thoughtworks.com>
2024-07-09 13:30:36 -0600

    Adjust header

--- src/components/Header/Header.stories.ts
+++ src/components/Header/Header.stories.ts
@@ -4,7 +4,14 @@ import Header from "./Header";
 const meta: Meta<typeof Header> = {
     title: "Component/Header",
     component: Header,
-    tags: ["autodocs"]
+    tags: ["autodocs"],
+    parameters: {
+        layout: "fullscreen", // Adjusts the layout. Options: 'fullscreen', 'padded', 'centered'
+        viewport: {
+            // Adjusts the viewport size
+            defaultViewport: "responsive" // Options include: 'responsive', 'iphone6', 'iphone6p', 'ipad', etc.
+        }
+    }
 };
 
 export default meta;
