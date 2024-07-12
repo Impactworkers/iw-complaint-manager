367a0524181e076f3bfd1a2df417ab080d2c12e5
justin anthony <justin.anthony@thoughtworks.com>
2024-07-11 19:00:07 -0600

    Create mock router for unit test

--- src/components/AppBar/mockRouter.tsx
+++ src/components/AppBar/mockRouter.tsx
@@ -1,5 +1,34 @@
 import { createContext } from "react";
 import { Decorator } from "@storybook/react";
+import {
+    AppRouterContext,
+    AppRouterInstance
+} from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+export type AppRouterContextProviderMockProps = {
+    router: Partial<AppRouterInstance>;
+    children: React.ReactNode;
+};
+
+export const AppRouterContextProviderMock = ({
+    router,
+    children
+}: AppRouterContextProviderMockProps): React.ReactNode => {
+    const mockedRouter: AppRouterInstance = {
+        back: jest.fn(),
+        forward: jest.fn(),
+        push: jest.fn(),
+        replace: jest.fn(),
+        refresh: jest.fn(),
+        prefetch: jest.fn(),
+        ...router
+    };
+    return (
+        <AppRouterContext.Provider value={mockedRouter}>
+            {children}
+        </AppRouterContext.Provider>
+    );
+};
 
 export const mockRouter = {
     pathname: "/",
