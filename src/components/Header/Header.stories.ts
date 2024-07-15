5b69dd190c549c4cfbd7f3bb4a931f5b096dfac3
Charles Woods <159830290+CwoodsTW@users.noreply.github.com>
2024-07-15 15:43:08 -0500

    storybook changes

--- src/components/Header/Header.stories.ts
+++ src/components/Header/Header.stories.ts
@@ -1,9 +1,16 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Header from "./Header";
+import { expect, userEvent, within } from "@storybook/test";
+import { redirect, getRouter } from "@storybook/nextjs/navigation.mock";
 
 const meta: Meta<typeof Header> = {
     title: "Component/Header",
     component: Header,
+    parameters: {
+        nextjs: {
+            appDirectory: true
+        }
+    },
     tags: ["autodocs"]
 };
 
@@ -11,3 +18,12 @@ export default meta;
 type Story = StoryObj<typeof Header>;
 
 export const Default: Story = {};
+
+export const ProfileIconClicked: Story = {
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        const avatar = await canvas.findByText("JA");
+
+        await userEvent.click(avatar);
+    }
+};
