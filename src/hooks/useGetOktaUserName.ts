ece21c41ec476e39aaa58f2da434a8715b606b54
justin anthony <justin.anthony@thoughtworks.com>
2024-07-19 10:27:33 -0600

    Revert "Add a condition to import a certain hook based on env"
    This reverts commit 4b507b8a03c497cf8bb7bae9213570baae1992d1.

--- src/hooks/useGetOktaUserName.ts
+++ src/hooks/useGetOktaUserName.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useOktaAuth } from "./useOktaAuth";
+import { useOktaAuth } from "@okta/okta-react";
 
 export interface UseGetOktaUserNameReturnType {
     userName: string;
--- src/hooks/useOktaAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useOktaAuth as realUseOktaAuth } from "@okta/okta-react";
-import { useOktaAuth as mockUseOktaAuth } from "@/mocks/mockContexts/useOktaAuthMock.";
-
-const useOktaAuth = process.env.STORYBOOK ? mockUseOktaAuth : realUseOktaAuth;
-
-export { useOktaAuth };
