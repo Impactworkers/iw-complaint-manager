9664ec77b32b4e7e1fe40e1dd730b9f585921be1
justin anthony <justin.anthony@thoughtworks.com>
2024-07-19 10:27:26 -0600

    Revert "Define paths based on environment"
    This reverts commit 92aba76b94433b4bead77aaf3d02e067abec73ed.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,4 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import { DefinePlugin } from 'webpack';
-import path from 'path';
-
-
 
 const config: StorybookConfig = {
     stories: [
@@ -22,22 +18,7 @@ const config: StorybookConfig = {
         name: "@storybook/nextjs",
         options: {}
     },
-    staticDirs: ["../public"],
-    webpackFinal: async (config) => {
-        config.plugins = config.plugins || [];
-        config.plugins.push(
-          new DefinePlugin({
-            'process.env.STORYBOOK': JSON.stringify(true),
-          })
-        );
-        config.resolve = config.resolve || {};
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          'next/navigation': path.resolve(__dirname, '../src/components/AppBar/storybookRouter.tsx'),
-        };
-    
-        return config;
-      },
+    staticDirs: ["../public"]
 };
 
 export default config;
